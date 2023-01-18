35f9c42616d26669cb09abc8c5afda9fd9151855
Tom McLaughlin <mclaughlintomd@gmail.com>
2023-01-18 16:17:23 -0600

    following sololearn

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const name = "David";
+const el = <p>Hello, {name}</p>;
 root.render(
-  <React.StrictMode>
-    <h1>Hello, React!</h1>,
-    <App />
-  </React.StrictMode>
+  el
 );
 
 // If you want to start measuring performance in your app, pass a function
