90ef1098d735cbf488d9306c5a1ff17d5f93862f
Tom McLaughlin <mclaughlintomd@gmail.com>
2023-01-20 13:47:41 -0600

    put original code commented out

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <h1>Hello, React!</h1>
+//     <App />
+//   </React.StrictMode>
+// );
 const name = "David";
 const el = <p>Hello, {name}</p>;
 root.render(
