dd77bc793316da82b12ad580af79a5d8d40636ad
Tom McLaughlin <mclaughlintomd@gmail.com>
2023-02-05 15:20:35 -0600

    updating learn functional elements

--- src/index.js
+++ src/index.js
@@ -11,19 +11,29 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //     <App />
 //   </React.StrictMode>
 // );
-let counter = 0;
-function show() {
-  const el = <p>{counter}</p>;
-  counter++;
-  root.render(
-    <React.StrictMode>
-      <h1>Hello, React!</h1>
-      {el}
-   </React.StrictMode>
-  );
-}
+// counter function show
+// let counter = 0;
+// function show() {
+//   const el = <p>{counter}</p>;
+//   counter++;
+//   root.render(
+//     <React.StrictMode>
+//       <h1>Hello, React!</h1>
+//       {el}
+//    </React.StrictMode>
+//   );
+// }
 
-setInterval(show, 1000);
+//setInterval(show, 1000);
+function Hello() {
+  return <h1>Hello world.</h1>;
+}
+const el = <Hello />
+root.render(
+  <React.StrictMode>
+    {el}
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
