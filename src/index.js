388e6f1743ad80aa324d92dce40efaeda590714f
Tom McLaughlin <mclaughlintomd@gmail.com>
2023-05-30 17:58:29 -0500

    updated with contact list

--- src/index.js
+++ src/index.js
@@ -48,19 +48,58 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 //   }
 
 //   return <form onSubmit={handleSubmit}>
-//     <input type="number" value={num} onChange={handleChange} />
+//     <input type ="number" value={num} onChange={handleChange} />
 //     <input type="submit" value="Add" />
 //     <p> Sum is {sum} </p>
 //   </form>;
 // }
 //
-function MyList(props) {
+function AddPersonForm(props) {
+  const [person, setPerson] = useState('');
+
+  function handleChange(e) {
+    setPerson(e.target.value);
+  }
+  
+  function handleSubmit(e) {
+    if(person !== '') {
+      props.handleSubmit(person);
+      setPerson('');
+    }
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text"
+        placeholder="Add new contact"
+        onChange={handleChange}
+        value={person} />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
+
+function PeopleList(props) {
   const arr = props.data;
   const listItems = arr.map((val, index) =>
-    <li key={index}>{val}</li>
+   <li key={index}>{val}</li>
+   );
+   return <ul>{listItems}</ul>;
+}
+
+function ContactManager(props) {
+  const [contacts, setContacts] = useState(props.data);
+  
+  function addPerson(name) {
+    setContacts([...contacts, name]);
+  }
+  
+  return (
+    <div>
+      <AddPersonForm handleSubmit={addPerson} />
+      <PeopleList data={contacts} />
+    </div>
   );
-  console.log("listItems: ", listItems);
-  return <ul>{listItems}</ul>;
 }
 // function component using useState and change handlers
 // function Converter() {
@@ -134,8 +173,8 @@ function MyList(props) {
 //     <Item name="Ice cream" price="24.00" />
 //   </div>
 // }
-const arr = ["A", "B", "C"];
-const el = <MyList data={arr} />;
+const contacts = ["James Smith", "Thomas Anderson", "Bruce Wayne"];
+const el = <ContactManager data={contacts} />;
 root.render(
   <React.StrictMode>
     {el}
