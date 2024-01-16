ce2031624f00528cca5cef902f83bb43c9152b58
realonlytomd <mclaughlintomd@gmail.com>
2024-01-16 16:19:14 -0600

    correct dl to ul

--- src/PeopleList.js
+++ src/PeopleList.js
@@ -5,13 +5,15 @@ function PeopleList(props) {
     const arr = props.data;
     console.log("arr: ", arr);
     const listItems = arr.map((val, index) =>
-        <div key={index}>
-        <dt>{val.name}</dt>
-        <dd>{val.age}</dd>
-        </div>
+        // <div key={index}>
+        // <dt>{val.name}</dt>
+        // <dd>{val.age}</dd>
+        //</div>  with a <dl> element below instead of ul
+        // or
+        <li key={index}>{val.name}  {val.age}</li>
      );
      console.log("listItems: ", listItems);
-     return <dl className="centerright">{listItems}</dl>;
+     return <ul className="centerright">{listItems}</ul>;
 }
 
 
